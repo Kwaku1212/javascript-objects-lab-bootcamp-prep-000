a313e68dcba763e9cb95fd699661ecbbdff2c8ad
Kwaku J <gyazi@live.com>
2018-04-13 23:12:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return new_object
 }
 
+<<<<<<< HEAD
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
@@ -21,3 +22,6 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   return object
 }
 
+=======
+fu
+>>>>>>> 8ae646f3bd4d8bf40be25abea5cf05b22a47f4f8
